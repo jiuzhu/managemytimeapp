4574d03b2727db7edbecf392668edfb811ca5d04
jiuzhu <jwang@juliawang-mac.gateway.pace.com>
2013-10-22 18:44:56 -0500

    Managemytimeapp Created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Managemytimeapp::Application.config.secret_key_base = 'ba712bd056d16bf5ec6511cca1f149785ae99787123c34f01b00db74bf5136335c3f15e7aa6a78652ee33ad0808a0b3f744571b76fe4b594909a4b057063bdcc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Managemytimeapp::Application.config.session_store :cookie_store, key: '_managemytimeapp_session'
