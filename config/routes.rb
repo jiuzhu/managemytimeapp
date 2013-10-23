1a0dbe074cd57689827d1cd4e4574ef26654d7c3
jiuzhu <jwang@juliawang-mac.gateway.pace.com>
2013-10-22 19:09:44 -0500

    Week 4 Homework ManageMyTimeApp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Managemytimeapp::Application.routes.draw do
+  get("/users", :controller => 'users', :action => 'new')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +40,7 @@ Managemytimeapp::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
